209ad85b1528631a7e275e55dcd28a4e62ade52d
Bruno Castro <miroldols@gmail.com>
2025-03-24 15:44:48 -0300

    style: remove redundant `items-center` class from GameController
    The `items-center` class was redundant as `justify-items-center` already centers the items within the grid. This change simplifies the CSS and improves readability.

--- src/components/GameController.tsx
+++ src/components/GameController.tsx
@@ -11,7 +11,7 @@ export default function GameController() {
   const { gameState, handleStart, handleCellClick, handleRestart, handleContinue } = useGameController();
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gradient-to-br from-gray-900 to-gray-800 text-white">
+    <div className="grid grid-rows-[20px_1fr_20px]  justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gradient-to-br from-gray-900 to-gray-800 text-white">
       <main className="flex flex-col gap-[32px] row-start-2 items-center">
         <h1 className="text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-purple-500">Jogo da Velha</h1>
         <Scoreboard gameState={gameState} />
